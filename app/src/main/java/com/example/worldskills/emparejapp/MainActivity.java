34bb4aac1fdcd06618088e1572abde895fcad7dd
WORLDSKILLS <juandiego1918@gmail.com>
2018-08-29 09:59:43 -0500

    JUAN

--- app/src/main/java/com/example/worldskills/emparejapp/MainActivity.java
+++ app/src/main/java/com/example/worldskills/emparejapp/MainActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
 
-    //oe
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
