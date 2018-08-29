5cfa18495bf53d777324c82b9a8e5d862e4451ec
WORLDSKILLS <juandiego1918@gmail.com>
2018-08-29 13:55:54 -0500

    JUAN

--- app/src/main/java/com/example/worldskills/emparejapp/activity/InicioActivity.java
+++ app/src/main/java/com/example/worldskills/emparejapp/activity/InicioActivity.java
@@ -3,11 +3,14 @@ package com.example.worldskills.emparejapp.activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 import com.example.worldskills.emparejapp.R;
 
 public class InicioActivity extends AppCompatActivity {
 
+
+    EditText player1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- app/src/main/java/com/example/worldskills/emparejapp/adapter/PuntajesAdapter.java
+++ app/src/main/java/com/example/worldskills/emparejapp/adapter/PuntajesAdapter.java
@@ -1,8 +1,30 @@
 package com.example.worldskills.emparejapp.adapter;
 
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AbsListView;
 
-public class PuntajesAdapter {
+public class PuntajesAdapter extends RecyclerView.Adapter<PuntajesAdapter.PuntajesHolderView> {
 
-    public class PuntajesHolder implements
+    @Override
+    public PuntajesHolderView onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(PuntajesHolderView holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class PuntajesHolderView extends RecyclerView.ViewHolder {
+        public PuntajesHolderView(View itemView) {
+            super(itemView);
+        }
+    }
 }
